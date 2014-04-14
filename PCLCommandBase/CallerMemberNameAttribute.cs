9fb3efdb4ab882e15707146e8e6f1bfdcee19614
Andrew Arnott <andrewarnott@gmail.com>
2014-04-14 08:35:02 -0700

    Targets Silverlight 5 as well.

---
+++ PCLCommandBase/CallerMemberNameAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace PCLCommandBase
+{
+    using System;
+
+    /// <summary>
+    /// A Silverlight compatible version of the 
+    /// System.Runtime.CompilerServices.CallerMemberNameAttribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
+    public sealed class CallerMemberNameAttribute : Attribute
+    {
+    }
+}
