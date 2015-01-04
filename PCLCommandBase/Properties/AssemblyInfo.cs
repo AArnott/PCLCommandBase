6f8f0f05693a65551ea26b93d1d70f222c7e08f0
Andrew Arnott <andrewarnott@gmail.com>
2015-01-03 17:45:54 -0800

    Increment to version 0.2.3.

--- PCLCommandBase/Properties/AssemblyInfo.cs
+++ PCLCommandBase/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2")]
-[assembly: AssemblyFileVersion("0.2.2")]
-[assembly: AssemblyInformationalVersion("0.2.2")]
+[assembly: AssemblyFileVersion("0.2.3")]
+[assembly: AssemblyInformationalVersion("0.2.3")]
